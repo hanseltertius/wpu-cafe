c9dbffa160123ebf76422d54e0fd20bc1df0d1b2
hanseltertius <hansel.tertius@gmail.com>
2025-04-02 00:28:53 +0700

    Modify Input component

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,5 +1,6 @@
 import styles from './Input.module.css';
 import useInputValue from '../../../hooks/useInputValue';
+import Error from '../Error';
 
 interface IPropTypes {
   id: string;
@@ -8,8 +9,9 @@ interface IPropTypes {
   type?: string;
   placeholder?: string;
   className?: string;
-  required?: boolean;
+  isRequired?: boolean;
   isSearch?: boolean;
+  callback?: () => void;
 }
 
 const Input = (props: IPropTypes) => {
@@ -20,10 +22,21 @@ const Input = (props: IPropTypes) => {
     placeholder,
     className,
     type = 'text',
-    required = false,
+    isRequired = false,
+    callback = () => {},
   } = props;
 
-  const { inputValue, setInputValue } = useInputValue();
+  const { inputValue, setInputValue } = useInputValue(callback);
+
+  const inputClassName = `${styles.input} ${
+    isRequired && inputValue.length === 0 ? styles['mandatory-border'] : ''
+  } ${className}`;
+
+  const getErrorMessage = (label?: string) => {
+    return label && label.length > 0
+      ? `${label} must not be empty`
+      : 'Input value must not be empty';
+  };
 
   return (
     <div className={`${styles['input-wrapper']}`}>
@@ -32,21 +45,13 @@ const Input = (props: IPropTypes) => {
         placeholder={placeholder}
         type={type}
         id={id}
-        className={`${styles.input} ${
-          required && inputValue.length === 0 ? styles['mandatory-border'] : ''
-        } ${className} `}
+        className={inputClassName}
         name={name}
         value={inputValue}
         onChange={setInputValue}
       ></input>
-      {required && inputValue.length > 0 ? (
-        ''
-      ) : (
-        <div className={styles['error-input']}>
-          {required && label && label.length > 0
-            ? `${label} must not be empty`
-            : 'Input value must not be empty'}
-        </div>
+      {isRequired && inputValue.length === 0 && (
+        <Error message={getErrorMessage(label)} />
       )}
     </div>
   );
--- src/hooks/useInputValue.tsx
+++ src/hooks/useInputValue.tsx
@@ -1,10 +1,15 @@
 import { useState } from 'react';
 
-const useInputValue = () => {
+const useInputValue = (callback?: () => void) => {
   const [inputValue, setInputValue] = useState('');
 
-  const onValueChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onValueChanged = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>,
+  ) => {
     setInputValue(e.target.value);
+    if (!!callback) callback();
   };
 
   return {
