4d2094687671ee5c60e8e4c90cd78b9d6414fd86
hanseltertius <hansel.tertius@gmail.com>
2025-04-03 00:06:39 +0700

    Create menu.ts

---
+++ src/types/menu.ts
@@ -0,0 +1,12 @@
+interface IMenu {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  image_url: string;
+  category: string;
+  is_available: boolean;
+  created_at: string;
+}
+
+export type { IMenu };
