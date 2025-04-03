d3cb6cb46e9ce29648b7c4942e8eb49cf5397b9a
hanseltertius <hansel.tertius@gmail.com>
2025-04-04 01:05:18 +0700

    Create OrderItem component

---
+++ src/components/ui/OrderItem/OrderItem.tsx
@@ -0,0 +1,30 @@
+import { ICart } from '../../../types/cart';
+import styles from './OrderItem.module.css';
+
+interface IPropTypes {
+  item: ICart;
+}
+
+const OrderItem = (props: IPropTypes) => {
+  const { item } = props;
+
+  return (
+    <div className={styles.container}>
+      <img
+        src={item.menuItem?.image_url}
+        alt={item.menuItem?.name}
+        className={styles.image}
+      />
+      <div className={styles.content}>
+        <div className={styles.item}>
+          <h2>{item.menuItem?.name}</h2>
+          <div>Qty: {item.quantity}</div>
+        </div>
+        <h3>${item.menuItem?.price}</h3>
+        <div>{item.notes}</div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderItem;
