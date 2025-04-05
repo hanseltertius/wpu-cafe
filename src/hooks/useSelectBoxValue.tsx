21bd0e4aefc66ffd92927eac56b6bb0eae25774c
hanseltertius <hansel.tertius@gmail.com>
2025-04-05 23:15:47 +0700

    Create useSelectBoxValue.tsx

---
+++ src/hooks/useSelectBoxValue.tsx
@@ -0,0 +1,24 @@
+import { useCallback } from 'react';
+import useCallbackState from './useCallbackState';
+
+const useSelectBoxValue = (initialValue: string) => {
+  const [selectBoxValue, setSelectBoxValueRaw] =
+    useCallbackState<string>(initialValue);
+
+  const setSelectBoxValue = useCallback(
+    (
+      e: React.ChangeEvent<HTMLSelectElement>,
+      callback?: (val: string) => void,
+    ) => {
+      setSelectBoxValueRaw(e.target.value, callback);
+    },
+    [setSelectBoxValueRaw],
+  );
+
+  return {
+    selectBoxValue,
+    setSelectBoxValue,
+  };
+};
+
+export default useSelectBoxValue;
