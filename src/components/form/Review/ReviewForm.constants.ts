47e0a27a18b27f600e5047168f1333d04b770702
hanseltertius <hansel.tertius@gmail.com>
2025-04-05 17:16:15 +0700

    Create ReviewForm component

---
+++ src/components/form/Review/ReviewForm.constants.ts
@@ -0,0 +1,11 @@
+import { IOption } from '../../../types/option';
+
+const ratings: IOption[] = [
+  { value: '1', name: '1 star' },
+  { value: '2', name: '2 stars' },
+  { value: '3', name: '3 stars' },
+  { value: '4', name: '4 stars' },
+  { value: '5', name: '5 stars' },
+];
+
+export { ratings };
---
+++ src/components/form/Review/ReviewForm.tsx
@@ -0,0 +1,88 @@
+import { FormEvent } from 'react';
+import Button from '../../ui/Button';
+import Input from '../../ui/Input';
+import Select from '../../ui/Select';
+import { ratings } from './ReviewForm.constants';
+import styles from './ReviewForm.module.css';
+import { IReview } from '../../../types/review';
+
+interface IPropTypes {
+  id: string;
+  menuItemId: string;
+}
+
+const ReviewForm = (props: IPropTypes) => {
+  const { id, menuItemId } = props;
+
+  /**
+   * TODO :
+   *
+   * - Layout
+   *  - reviewer name
+   *  - ratings (1 - 5)
+   *  - comment
+   *
+   * 1. bikin layout container
+   * i. Reviewer Name
+   * ii. Ratings (sementara pake select box), paling coba yang star
+   *
+   */
+
+  /**
+   *
+   * Sample Payload :
+   * {
+   *    "menuItemId": "item-uuid",
+   *    "reviewerName": "John Smith",
+   *    "rating": 5,
+   *    "comment": "Excellent coffee, would order again!"
+   * }
+   *
+   */
+
+  const handleReview = (event: FormEvent) => {
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+
+    const reviewer_name = form.reviewerName.value as string;
+    if (!reviewer_name) return;
+
+    const rating = form.rating.value as number;
+    const comment = form.comment.value as string;
+
+    const payload: IReview = {
+      menu_item_id: menuItemId,
+      reviewer_name,
+      rating,
+      comment,
+    };
+
+    // TODO : call API
+
+    console.log('payload : ', payload);
+  };
+
+  // sementara pake select dulu, baru pake stars
+  return (
+    <form id={id} className={styles.form} onSubmit={handleReview}>
+      <Input
+        id="reviewerName"
+        label="Reviewer Name"
+        placeholder="Reviewer Name"
+        isRequired
+      />
+      <Select id="rating" label="Rating" options={ratings} />
+      <Input
+        id="comment"
+        label="Comment"
+        placeholder="Add a comment (optional)"
+      />
+      <Button id="submit" type="submit">
+        Submit a review
+      </Button>
+    </form>
+  );
+};
+
+export default ReviewForm;
