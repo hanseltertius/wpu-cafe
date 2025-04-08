1e4e0d66a5eeec657f73a8183d63a0924d915fd3
hanseltertius <hansel.tertius@gmail.com>
2025-04-08 22:21:24 +0700

    Create order.service.ts

---
+++ src/services/order.service.ts
@@ -0,0 +1,71 @@
+import { environment } from '../constants/environment';
+import { ICustomerInfo } from '../types/customerInfo';
+import axiosInstance from '../utils/axios';
+import { getLocalStorage } from '../utils/storage';
+
+export const getOrders = async (
+  page: number,
+  pageSize: number,
+  search?: string,
+  status?: string,
+  sortBy?: string,
+  sortOrder?: string,
+) => {
+  let url = `${environment.API_URL}/orders?page=${page}&pageSize=${pageSize}`;
+
+  if (!!search) url += `&search=${search}`;
+  if (!!status) url += `&category=${status}`;
+  if (!!sortBy) url += `&sortBy=${sortBy}`;
+  if (!!sortOrder) url += `&sortOrder=${sortOrder}`;
+
+  const result = await axiosInstance.get(url, {
+    headers: {
+      Authorization: `Bearer ${getLocalStorage('auth')}`,
+    },
+  });
+
+  return result;
+};
+
+export const getOrderDetail = async (id: string) => {
+  const url = `${environment.API_URL}/orders/${id}`;
+  const result = await axiosInstance.get(url, {
+    headers: {
+      Authorization: `Bearer ${getLocalStorage('auth')}`,
+    },
+  });
+  return result;
+};
+
+export const createOrder = async (payload: ICustomerInfo) => {
+  const url = `${environment.API_URL}/orders`;
+  const result = await axiosInstance.post(url, payload, {
+    headers: {
+      Authorization: `Bearer ${getLocalStorage('auth')}`,
+    },
+  });
+
+  return result;
+};
+
+export const updateOrder = async (id: string, payload: ICustomerInfo) => {
+  const url = `${environment.API_URL}/orders/${id}`;
+  const result = await axiosInstance.put(url, payload, {
+    headers: {
+      Authorization: `Bearer ${getLocalStorage('auth')}`,
+    },
+  });
+
+  return result;
+};
+
+export const deleteOrder = async (id: string) => {
+  const url = `${environment.API_URL}/orders/${id}`;
+  const result = await axiosInstance.delete(url, {
+    headers: {
+      Authorization: `Bearer ${getLocalStorage('auth')}`,
+    },
+  });
+
+  return result;
+};
