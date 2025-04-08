8f96030a9a8bd2a8eeec8c1bdbf9092972b2a710
hanseltertius <hansel.tertius@gmail.com>
2025-04-08 21:49:15 +0700

    Create menu.service.ts

---
+++ src/services/menu.service.ts
@@ -0,0 +1,28 @@
+import { environment } from '../constants/environment';
+import axiosInstance from '../utils/axios';
+
+export const getMenu = async (
+  page: number,
+  pageSize: number,
+  search?: string,
+  category?: string,
+  sortBy?: string,
+  sortOrder?: string,
+) => {
+  let url = `${environment.API_URL}/menu?page=${page}&pageSize=${pageSize}`;
+
+  if (!!search) url += `&search=${search}`;
+  if (!!category) url += `&category=${category}`;
+  if (!!sortBy) url += `&sortBy=${sortBy}`;
+  if (!!sortOrder) url += `&sortOrder=${sortOrder}`;
+
+  const result = await axiosInstance.get(url);
+
+  return result;
+};
+
+export const getMenuDetail = async (id: string) => {
+  const url = `${environment.API_URL}/menu/${id}`;
+  const result = await axiosInstance.get(url);
+  return result;
+};
