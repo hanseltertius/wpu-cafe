c76768501a8230dd3b299843408c269cc32fd3bf
hanseltertius <hansel.tertius@gmail.com>
2025-04-10 10:41:41 +0700

    Update ScreenStore.tsx
    Update to check if the device is desktop

--- src/stores/ScreenStore.tsx
+++ src/stores/ScreenStore.tsx
@@ -1,14 +1,19 @@
 import { create } from 'zustand';
 
 interface ScreenStore {
-  isMobile: boolean;
-  setIsMobile: (matches: boolean) => void;
+  isDesktop: boolean;
+  setIsDesktop: (matches: boolean) => void;
 }
 
+const isInitialDesktop =
+  typeof window !== 'undefined'
+    ? window.matchMedia('(min-width: 1024px)').matches
+    : false;
+
 const useScreenStore = create<ScreenStore>((set) => ({
-  isMobile: false,
-  setIsMobile: (matches: boolean) => {
-    set(() => ({ isMobile: matches }));
+  isDesktop: isInitialDesktop,
+  setIsDesktop: (matches: boolean) => {
+    set(() => ({ isDesktop: matches }));
   },
 }));
 
