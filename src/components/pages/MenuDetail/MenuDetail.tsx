b8f82d1179fa2521299eb20152c5a0933413bc81
hanseltertius <hansel.tertius@gmail.com>
2025-04-11 23:25:23 +0700

    Complete MenuDetail

--- src/components/pages/MenuDetail/MenuDetail.tsx
+++ src/components/pages/MenuDetail/MenuDetail.tsx
@@ -1,148 +1,76 @@
+import { useNavigate, useParams } from 'react-router-dom';
 import useScreenStore from '../../../stores/ScreenStore';
 import styles from './MenuDetail.module.css';
+import { ButtonColor } from '../../ui/Button/Button.constants';
+import Button from '../../ui/Button';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { getMenuDetail } from '../../../services/menu.service';
+import { useEffect } from 'react';
+import Loading from '../../ui/Loading';
+import { IReview } from '../../../types/review';
+import Review from '../../ui/Review';
 
 const MenuDetail = () => {
-  /**
-   * TODO :
-   * layout:
-   * a. menu item information => itu coba untuk bikin image, dan juga scrollable content
-   * b. reviews list
-   *
-   */
+  const queryClient = useQueryClient();
 
+  const { id } = useParams();
   const { isDesktop } = useScreenStore();
+
+  const menuId = !!id ? id : '0';
+
+  useEffect(() => {
+    return () => {
+      queryClient.invalidateQueries({ queryKey: ['orderDetail'] });
+    };
+  }, []);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['orderDetail', menuId],
+    queryFn: () => getMenuDetail(menuId),
+    staleTime: 0,
+  });
+
+  const navigate = useNavigate();
+
+  const handleBackButton = () => {
+    navigate('/menus');
+  };
+
+  const getMenuDetailData = () => {
+    return !!data ? data : {};
+  };
+
+  const getMenuDetailReviews = () => {
+    const menuDetail = getMenuDetailData();
+
+    if (!!menuDetail) {
+      const reviews = menuDetail.reviews;
+      if (!!reviews) return !!reviews.items ? reviews.items : [];
+    } else return [];
+  };
 
   return (
     <main className="layout">
+      {isLoading && <Loading />}
       <header className="layout-header">
         <h1>Menu Detail</h1>
-        <div>Test</div>
+        <Button id="back" onClick={handleBackButton} color={ButtonColor.DANGER}>
+          Back
+        </Button>
       </header>
       <section className="layout-content">
         <section className={styles['menu-detail-information-container']}>
           <div></div>
           <section className="scroll-wrapper">
             <section className={styles['menu-detail-information']}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
-              aperiam fugiat, est temporibus obcaecati voluptatum, eum enim
-              deleniti reprehenderit fugit dicta vel recusandae eos, reiciendis
-              exercitationem id iure facere et quam maiores. Perferendis tenetur
-              quibusdam animi eaque obcaecati ratione autem aliquam minus
-              repellendus est ullam, dolores in. Minima, eaque adipisci nam
-              earum itaque doloribus? Aperiam explicabo quod maxime molestias et
-              doloribus nisi excepturi rem! Minima amet id, perferendis quasi
-              fuga voluptatem at, nisi obcaecati ad illo cum incidunt
-              blanditiis. Numquam culpa vel facilis dicta maiores, dolor
-              corporis iusto tempore sunt nam, impedit ullam doloribus repellat
-              laudantium eaque dolorum ab error illo temporibus eos officia
-              repudiandae. Perspiciatis, culpa, facilis vero dicta incidunt
-              tenetur est ratione, ea itaque corporis consectetur velit?
-              Quibusdam iusto voluptates iste praesentium nulla obcaecati
-              similique, libero, alias autem ullam veniam dolorem placeat
-              numquam repudiandae aut dolor fuga eos nobis repellat blanditiis
-              vitae! Sit delectus nostrum quasi, ut a asperiores dignissimos
-              repellat! Aliquid, minima? Consequuntur impedit dignissimos
-              possimus rerum accusamus reiciendis harum soluta fugiat. Quasi
-              aliquid nulla consequatur natus, dolorum dolore explicabo quia
-              voluptatum nam facilis debitis temporibus itaque. Aut dolorum
-              dolores adipisci ducimus, tempora enim alias voluptate. Facere
-              dolorum recusandae delectus praesentium animi magnam esse labore,
-              iure id est sequi cumque ipsa, architecto odit! Id repellat quo,
-              dolor in quia omnis dicta praesentium soluta quidem quasi cum
-              velit dolore natus nisi, corrupti aut nesciunt ex laborum
-              doloremque, explicabo facilis deleniti aperiam fugiat? Eum dolorem
-              eligendi corrupti voluptatum dolores magnam rerum, esse vero ad,
-              similique hic error molestias sed harum. Tempore et dicta iusto
-              laudantium culpa nam obcaecati eligendi molestias repellat
-              tempora. Quos doloremque, optio cum magni obcaecati laudantium
-              libero explicabo quis? Voluptate ducimus dolore illo, ratione
-              explicabo nobis impedit, reiciendis nam veniam velit repellat
-              obcaecati fugit doloribus minus nesciunt maxime nihil, alias quod
-              voluptas sit ex at eos quia? In ut at magnam rem officiis,
-              nesciunt deserunt eos quisquam architecto autem! Cum iusto,
-              voluptatem laudantium omnis praesentium similique dolor
-              perferendis. Placeat animi, aspernatur obcaecati ex praesentium
-              eligendi voluptates similique vitae iste provident consequuntur
-              culpa blanditiis autem amet accusantium perspiciatis quidem
-              ratione. Odio temporibus deleniti rerum repellat officiis dolorem
-              aperiam voluptatibus? Aperiam assumenda voluptas ab cum neque
-              debitis maxime ratione deserunt, eveniet dolor explicabo sint,
-              laboriosam consectetur hic velit vero? Minima saepe velit error,
-              quam sequi veritatis alias molestiae ut eum ullam minus asperiores
-              non ducimus qui obcaecati porro provident libero facilis maiores
-              aperiam id commodi quo assumenda. Omnis tempora eaque perspiciatis
-              tempore placeat in officiis libero, quod aperiam culpa dolore sed
-              numquam natus inventore perferendis sit rem? Sunt impedit
-              laboriosam voluptatibus, vero molestias voluptatem quidem ducimus
-              incidunt nostrum neque expedita distinctio, aut recusandae, iusto
-              nesciunt animi quaerat laborum. Recusandae nulla alias voluptate
-              quo rem consequatur nisi reiciendis reprehenderit voluptatum, nam
-              architecto expedita autem. Ut sit architecto sequi perspiciatis
-              reiciendis ea numquam quis, beatae accusantium quidem quae id
-              consequuntur! Doloremque, voluptas maiores. Incidunt quod
-              inventore voluptatibus at in ipsum iste reiciendis voluptas totam
-              ea dolor earum laboriosam error perferendis et soluta porro
-              corrupti qui velit rem nisi ab, tenetur aut! Provident eaque
-              fugiat reiciendis consequuntur facilis minus odit modi. Aliquid,
-              quas! Repellat perferendis iste obcaecati, laboriosam dolorem
-              harum tenetur inventore consequuntur natus cum provident sapiente
-              similique aperiam magnam assumenda cumque minima aliquid adipisci
-              suscipit nam optio distinctio? Recusandae officiis at iusto,
-              praesentium dicta harum deserunt sequi fuga, quia, ab iure
-              dolores? Dolorum delectus quo aliquid at, obcaecati nam modi
-              minima in eaque expedita, doloribus illum ratione necessitatibus,
-              magnam cumque harum ut nobis porro! Omnis quas soluta ut nam
-              voluptatibus delectus cum voluptatem minima totam, vitae
-              architecto magni qui illo sint vel fuga doloremque dignissimos
-              rerum, a est tenetur at non eveniet consequatur! Atque reiciendis
-              sit eum numquam. Officia aliquid beatae cupiditate ad hic quam in
-              vitae nam repudiandae tenetur quae dolorum accusamus odit rem sed
-              tempore blanditiis saepe enim, et iste distinctio dolores!
-              Necessitatibus dicta quod exercitationem dolorem quaerat. Numquam
-              debitis eos totam adipisci asperiores, voluptates a nihil iure
-              cumque quia odit quis atque dignissimos culpa harum cum quisquam
-              eligendi ea veritatis delectus sequi deleniti laudantium. Quidem
-              modi voluptatem cupiditate quos adipisci libero illum autem
-              commodi deleniti. Perferendis ab amet quisquam beatae aliquam.
-              Officia magnam ipsam possimus sint cum. Cupiditate quas doloremque
-              doloribus voluptates numquam aliquid laudantium vero nihil
-              perferendis, perspiciatis consequuntur beatae eum quae quibusdam
-              aperiam culpa eveniet consectetur sunt debitis magni deserunt
-              deleniti obcaecati! Fuga similique architecto dolorem repudiandae
-              hic consequatur possimus voluptates dolore quaerat sequi, iure ut
-              deserunt veritatis quae corrupti velit recusandae numquam odio sed
-              eos alias? Expedita rem voluptate eius eaque at natus deleniti
-              perferendis eveniet ea perspiciatis, veritatis enim vel labore
-              facere nam officia reprehenderit possimus est, dignissimos
-              exercitationem. Maiores, neque debitis. Omnis perspiciatis
-              officiis ad provident rerum veritatis possimus exercitationem est
-              quae quisquam magni numquam maiores sit aperiam labore asperiores
-              molestiae ex animi excepturi iusto cumque, sunt ea! Quo
-              consequatur corporis perferendis delectus iusto quos recusandae
-              ipsum laudantium consectetur tempora tenetur nesciunt, omnis
-              perspiciatis. Aliquam impedit incidunt veniam eaque omnis minus
-              animi excepturi cupiditate, sunt ipsam quis saepe provident
-              tenetur ad temporibus, doloribus ipsum velit accusamus nulla.
-              Maiores corporis velit eligendi incidunt ipsa saepe, sunt corrupti
-              quia. Debitis veritatis deserunt architecto suscipit voluptatum
-              aliquid vitae laudantium fugit sapiente itaque aliquam porro optio
-              ipsam vel facere eius fuga, consectetur, unde, soluta consequuntur
-              in ab ratione quae reprehenderit. Dolorum deleniti molestiae,
-              veniam enim explicabo ipsam nihil laudantium! Neque corporis,
-              ratione debitis delectus minima quasi architecto sequi velit
-              ducimus deleniti accusantium illo ipsam beatae tenetur earum in a
-              unde tempora magnam quis! Veritatis, consectetur. Error, esse non!
-              Sequi obcaecati necessitatibus rem, odio amet at dignissimos!
-              Fugit maxime repellendus laboriosam neque omnis placeat nam sit
-              nesciunt provident vitae, nobis eos optio saepe. Ipsam autem,
-              harum consequuntur aperiam, repellendus repudiandae dolores maxime
-              vel accusantium doloribus quas ut quae illo magnam corrupti
-              aliquam deleniti incidunt enim! Porro dolores temporibus deserunt
-              error fugit voluptatem atque quidem praesentium quasi, esse nisi
-              dolor, saepe maiores, accusamus reprehenderit consequuntur. Ea
-              sint debitis maiores non voluptatibus optio, adipisci itaque
-              sapiente, praesentium expedita maxime nulla voluptatem eum vitae
-              nobis ullam rem molestiae est provident!
+              {!!data && !!data.menuItem && (
+                <div className={styles['menu-detail-content']}>
+                  <img src={data.menuItem.image_url} className={styles.image} />
+                  <h2>{data.menuItem.name}</h2>
+                  <div>{data.menuItem.description}</div>
+                  <div>${data.menuItem.price}</div>
+                </div>
+              )}
             </section>
           </section>
         </section>
@@ -151,30 +79,9 @@ const MenuDetail = () => {
             <h2>Reviews</h2>
             <section className="scroll-wrapper">
               <section className={styles['menu-detail-reviews']}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
-                aut, dignissimos aliquam modi reiciendis itaque minima natus
-                illo quis eveniet ipsam quae similique sint deserunt quidem
-                molestias molestiae voluptates atque optio fugit illum facilis
-                voluptatum. Voluptates sint nemo quod. Ipsam, omnis veritatis
-                eveniet corrupti tenetur doloremque ipsa ullam aperiam eaque
-                quas architecto explicabo magni laudantium facere, libero ab
-                deserunt exercitationem enim quod consequuntur itaque cum
-                dolores, quaerat labore! Vero natus, distinctio maiores
-                consequuntur esse corporis doloribus dolore architecto iure, qui
-                incidunt autem aperiam ab, nam dolor exercitationem voluptate ex
-                ullam! Laborum quasi iure sint tenetur. Nostrum sequi blanditiis
-                quaerat ea, consequatur impedit quod eos vel possimus
-                dignissimos a eveniet quisquam dolorem saepe quidem illum
-                incidunt vitae voluptas aliquid necessitatibus aspernatur. Magni
-                blanditiis nemo officiis quia porro ad ab quo a deleniti est et
-                facere consequuntur laudantium necessitatibus, ea dolor
-                dignissimos itaque incidunt pariatur ex. Sed illo eveniet
-                similique voluptate! Distinctio molestias amet libero suscipit
-                at porro nulla soluta facere debitis corrupti, totam eum tenetur
-                aliquam corporis quod velit in iure dignissimos atque? Ab nemo
-                facere vel officia tenetur deserunt nulla eos maxime, vitae
-                aliquid ea exercitationem ad alias laboriosam fugiat quasi fuga
-                rerum veniam itaque perferendis a ullam aut? Dignissimos.
+                {getMenuDetailReviews().map((item: IReview) => (
+                  <Review review={item} />
+                ))}
               </section>
             </section>
           </section>
