f8befe9b28525153a23061c26c56d59e121baa44
hanseltertius <hansel.tertius@gmail.com>
2025-04-12 14:49:54 +0700

    Update MenuList.tsx
    Impelment add to cart and review

--- src/components/pages/MenuList/MenuList.tsx
+++ src/components/pages/MenuList/MenuList.tsx
@@ -15,9 +15,15 @@ import { IMenu } from '../../../types/menu';
 import Menu from '../../ui/Menu';
 import CustomerInfo from '../../form/CustomerInfo';
 import useCartStore from '../../../stores/CartStore';
+import Popup from '../../modal/Popup';
+import AddToReview from '../../form/AddToReview';
+import Loading from '../../ui/Loading';
+import useLoadingStore from '../../../stores/LoadingStore';
+import AddToCart from '../../form/AddToCart';
 
 const MenuList = () => {
   const { isDesktop } = useScreenStore();
+  const { isProcessing } = useLoadingStore();
 
   const navigate = useNavigate();
 
@@ -27,23 +33,24 @@ const MenuList = () => {
   const customerName = useInputValue('');
   const tableNumber = useSelectBoxValue('1');
 
-  // const [isProcessingData, setIsProcessingData] = useState(false);
-
   const cartStore = useCartStore();
 
   const [keyword, setKeyword] = useState('');
+  const [menuId, setSelectedMenuId] = useState('');
+  const [menuName, setMenuName] = useState('');
+  const [isReviewOpen, setIsReviewOpen] = useState(false);
+  const [isAddToCartOpen, setIsAddToCartOpen] = useState(false);
 
   const { data } = useQuery({
     queryKey: ['menuList', keyword, categoryInput.selectBoxValue],
-    queryFn: () =>
-      getMenus(
-        1,
-        25,
-        keyword,
+    queryFn: () => {
+      const categorySearch =
         categoryInput.selectBoxValue === 'All'
           ? ''
-          : categoryInput.selectBoxValue,
-      ),
+          : categoryInput.selectBoxValue;
+
+      return getMenus(1, 25, keyword, categorySearch);
+    },
   });
 
   const handleBackButton = () => {
@@ -74,8 +81,33 @@ const MenuList = () => {
     if (cartStore.carts.length === 0) return;
   };
 
+  const handleAddNewReview = (id: string) => {
+    setSelectedMenuId(id);
+    setIsReviewOpen(true);
+  };
+
+  const handleCloseReview = () => {
+    setSelectedMenuId('');
+    setIsReviewOpen(false);
+  };
+
+  const handleAddToCart = (id: string) => {
+    const menu = getMenuList().find((i: IMenu) => i.id === id);
+
+    setMenuName(menu.name);
+    setSelectedMenuId(id);
+    setIsAddToCartOpen(true);
+  };
+
+  const handleCloseAddToCart = () => {
+    setMenuName('');
+    setSelectedMenuId('');
+    setIsAddToCartOpen(false);
+  };
+
   return (
     <main className="layout">
+      {isProcessing && <Loading />}
       <header className="layout-header">
         <h1>Menu List</h1>
         <Button id="back" onClick={handleBackButton} color={ButtonColor.DANGER}>
@@ -108,7 +140,11 @@ const MenuList = () => {
             <section className="grid">
               {getMenuList().map((item: IMenu) => (
                 <div key={item.id}>
-                  <Menu menu={item} />
+                  <Menu
+                    menu={item}
+                    handleAddNewReview={handleAddNewReview}
+                    handleAddToCart={handleAddToCart}
+                  />
                 </div>
               ))}
             </section>
@@ -137,6 +173,34 @@ const MenuList = () => {
           <div>Test</div>
         </div>
       )}
+      <Popup
+        isOpen={isReviewOpen}
+        title="Add New Review"
+        width="500px"
+        height="500px"
+        handleClose={handleCloseReview}
+      >
+        <AddToReview
+          id="add-to-review"
+          menuItemId={menuId}
+          handleClosePopup={handleCloseReview}
+        />
+      </Popup>
+
+      <Popup
+        isOpen={isAddToCartOpen}
+        title="Add to Cart"
+        width="500px"
+        height="500px"
+        handleClose={handleCloseAddToCart}
+      >
+        <AddToCart
+          id="add-to-cart"
+          menuItemId={menuId}
+          name={menuName}
+          handleClosePopup={handleCloseAddToCart}
+        />
+      </Popup>
     </main>
   );
 };
