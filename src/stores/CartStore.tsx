7cc790f162011d40d24c414da3973c9b13709f5e
hanseltertius <hansel.tertius@gmail.com>
2025-04-12 14:47:08 +0700

    Update CartStore.tsx
    Modify cart implementation

--- src/stores/CartStore.tsx
+++ src/stores/CartStore.tsx
@@ -3,47 +3,44 @@ import { ICart } from '../types/cart';
 
 interface CartStore {
   carts: ICart[];
-  handleAddToCart: (
-    type: 'increment' | 'decrement',
-    id: string,
-    name: string,
-  ) => void;
+  addItemToCart: (cart: ICart) => void;
+  editItemInCart: (id: string, cart: ICart) => void;
   removeCart: (id: string) => void;
 }
 
 const useCartStore = create<CartStore>((set, get) => ({
   carts: [],
-  handleAddToCart: (type, id, name) => {
-    const { carts, removeCart } = get();
-    const itemIsInCart = carts.find((item) => item.menuItemId === id);
-
-    if (type === 'increment') {
-      if (itemIsInCart) {
-        set({
-          carts: carts.map((item) =>
-            item.menuItemId === id
-              ? { ...item, quantity: item.quantity + 1 }
-              : item,
-          ),
-        });
-      } else {
-        set({
-          carts: [...carts, { menuItemId: id, name, quantity: 1 }],
-        });
-      }
-    } else {
-      if (itemIsInCart && itemIsInCart.quantity <= 1) {
-        removeCart(id);
-      } else {
-        set({
-          carts: carts.map((item) =>
-            item.menuItemId === id
-              ? { ...item, quantity: item.quantity - 1 }
-              : item,
-          ),
-        });
-      }
-    }
+  addItemToCart: (cart) => {
+    const { carts } = get();
+    set({
+      carts: [...carts, cart],
+    });
+  },
+  incrementItemInCart: (id: string) => {
+    const { carts } = get();
+    set({
+      carts: carts.map((item) =>
+        item.menuItemId === id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item,
+      ),
+    });
+  },
+  decrementItemInCart: (id: string) => {
+    const { carts } = get();
+    set({
+      carts: carts.map((item) =>
+        item.menuItemId === id
+          ? { ...item, quantity: item.quantity - 1 }
+          : item,
+      ),
+    });
+  },
+  editItemInCart: (id: string, cart: ICart) => {
+    const { carts } = get();
+    set({
+      carts: carts.map((item) => (item.menuItemId === id ? cart : item)),
+    });
   },
   removeCart: (id) => {
     const { carts } = get();
