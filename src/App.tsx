0dd7fe0e7a6f618bcd9a0a264818186ec6fc3b06
hanseltertius <hansel.tertius@gmail.com>
2025-04-14 23:56:34 +0700

    Update App.tsx
    Implement media query device change

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,25 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import { ToastContainer } from 'react-toastify';
+import { useEffect } from 'react';
+import useScreenStore from './stores/ScreenStore';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const { setIsDesktop } = useScreenStore();
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(min-width: 1024px)');
+
+    const handleChange = (event: MediaQueryListEvent) => {
+      setIsDesktop(event.matches);
+    };
+
+    mediaQuery.addEventListener('change', handleChange);
+    return () => mediaQuery.removeEventListener('change', handleChange);
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
